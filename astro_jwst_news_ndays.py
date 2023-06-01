1dbd5b01fc4cf5b1a74462ebaec178237c58cbf1
yuval <yuval@lenovo.2020>
2023-06-01 20:25:40 +0300

    comments added

--- astro_jwst_news_ndays.py
+++ astro_jwst_news_ndays.py
@@ -1,19 +1,23 @@
+'''
+The code reates a web page to view the latest JWST images
+@Author: Yuval Harpaz
+'''
+# requires pandas as well, no need to import
 import astropy
 from astroquery.mast import Observations
-# from astro_utils import *
-# from astropy.io import ascii
-# table = ascii.read('tmp.csv')
-# table = table.sort(keys='t_obs_release')
+
+
+# n days to look back for new releases
 n = 14
 print(f'reading {n} days')
-# table = last_n_days(n=n, html=False, products=False)
-
 end_time = astropy.time.Time.now().mjd
 start_time = end_time - n
+## Query MAST database for level 3 JWST images
 table = Observations.query_criteria(obs_collection="JWST",
-                                                t_obs_release=[start_time, end_time],
-                                                calib_level=3,
-                                                dataproduct_type="image")
+                                    t_obs_release=[start_time, end_time],
+                                    calib_level=3,
+                                    dataproduct_type="image")
+## make sure images are public, science, no Calibration in title
 if len(table) > 0:
     table = table[table['intentType'] == 'science']
     if len(table) > 0:
@@ -21,8 +25,8 @@ if len(table) > 0:
         if len(table) > 0:
             tdf = table.to_pandas()
             table = table[list(~tdf['obs_title'].str.contains("alibration"))]
-
-
+# Rewrite the web page if there's any data from the last 14 days. Set titles to show description when
+# hover over images
 if len(table) == 0:
     raise Exception('no new images')
 table = table.to_pandas()
@@ -42,38 +46,8 @@ for iimg in range(len(table)):  # min([len(table), n])):
            'proposal: ' + str(table['proposal_id'][iimg]) + '\n' + \
            jpg + '\n' + time[:16]
     date_prev = date
-
     page = page + '\n<img src="https://mast.stsci.edu/portal/Download/file/JWST/product/' + jpg + f'" title="{desc}">'
 page = page + '\n</body>\n</html>\n'
 with open('docs/news_by_date.html', "w") as text_file:
     text_file.write(page)
 
-# mir = plt.imread('/home/innereye/JWST/ngc_2835/ngc2835_miri.png')
-# nir = plt.imread('/home/innereye/JWST/ngc_2835/ngc2835_nircam.png')
-# mir = rgb2cmyk(mir)
-# nir = rgb2cmyk(nir)
-# mix = np.clip(nir+mir, 0, 255)  # .astype('uint8')
-# mix = cmyk2rgb(mix).astype('uint8')
-# plt.imshow(mix)
-#
-# ##
-# pow = 0.75
-# r = np.zeros((100,100,3))
-# r[40:60,40:60,0] = pow
-# b = np.zeros((100,100,3))
-# b[50:70,50:70,2] = pow
-# mix = 1 - (((1-r)**2 + (1-b)**2)/2)**0.5
-# # mix = 1 - (((1-r)**2 + (1-b)**2)/3)
-# plt.imshow(mix)
-# #
-# # c = rgb2cmyk(r)
-# # fill = 0
-# # c[np.isnan(c)] = fill
-# # y = rgb2cmyk(b)
-# # y[np.isnan(y)] = fill
-# # # miks = c
-# # # miks[..., :3] = c[..., :3] + y[..., :3]
-# # # mix = cmyk2rgb(miks)
-# # mix = 255-y-c
-# # mix = np.clip(mix, 0, 255).astype('uint8')
-# #
