c020bdede92b1aa0801fb43548489d5e3e6481a0
yuval <yuval@lenovo.2020>
2023-06-10 19:13:52 +0300

    rearrange

--- mastodon_bot.py
+++ mastodon_bot.py
@@ -1,17 +1,27 @@
 from mastodon import Mastodon
 import os
+# import sys
 #   Set up Mastodon
-local = '/home/innereye/astro/'
-if os.path.isdir(local):
-    os.chdir(local)
-    oauth = 'token.secret'
-    loc = 'local'
-else:
-    oauth = os.environ['OAuth']
-    loc = 'github'
-mastodon = Mastodon(
-    access_token=oauth,
-    api_base_url='https://botsin.space/'
-)
 
-mastodon.status_post('script test '+loc)
+
+def connect_bot():
+    local = '/home/innereye/astro/'
+    if os.path.isdir(local):
+        os.chdir(local)
+        oauth = 'token.secret'
+        loc = 'local'
+    else:
+        oauth = os.environ['OAuth']
+        loc = 'github'
+    masto = Mastodon(
+        access_token=oauth,
+        api_base_url='https://botsin.space/'
+    )
+    return masto, loc
+
+
+if __name__ == "__main__":
+    masto, loc = connect_bot()
+    # metadata = masto.media_post("pics/NGC1365_miri.png", "image/png")
+    # masto.status_post('toot image test '+loc, media_ids=metadata["id"])
+    masto.status_post(sys.argv[1])
