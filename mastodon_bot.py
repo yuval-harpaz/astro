78bf7441d5b2585b1a3a5cc464cfe63103478599
yuval <yuval@lenovo.2020>
2023-06-10 12:25:45 +0300

    allow local remote

--- mastodon_bot.py
+++ mastodon_bot.py
@@ -1,9 +1,17 @@
 from mastodon import Mastodon
-
+import os
 #   Set up Mastodon
+local = '/home/innereye/astro/'
+if os.path.isdir(local):
+    os.chdir(local)
+    oauth = 'token.secret'
+    loc = 'local'
+else:
+    oauth = os.environ['OAuth']
+    loc = 'github'
 mastodon = Mastodon(
-    access_token='token.secret',
+    access_token=oauth,
     api_base_url='https://botsin.space/'
 )
 
-mastodon.status_post("script test")
+mastodon.status_post('script test '+loc)
