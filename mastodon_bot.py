97987e1872539e5bd517acd32e3f3dbc008ac61f
yuval <yuval@lenovo.2020>
2023-06-10 09:07:04 +0300

    manual toot 1

--- mastodon_bot.py
+++ mastodon_bot.py
@@ -1,1 +1,9 @@
+from mastodon import Mastodon
 
+#   Set up Mastodon
+mastodon = Mastodon(
+    access_token='token.secret',
+    api_base_url='https://botsin.space/'
+)
+
+mastodon.status_post("script test")
