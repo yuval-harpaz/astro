629279cacb60abec1cba71e81ab0b4fedac67b35
yuval <yuval@lenovo.2020>
2025-02-25 08:38:04 +0200

    upload

---
+++ HOPS383.py
@@ -0,0 +1,3 @@
+from astro_utils import *
+auto_plot('HOPS383', exp = 'log', method='rrgggbb', png='rgb1m+deband.jpg',
+          adj_args={'factor':1}, func=None, fill=True, pkl=False, deband=True)
