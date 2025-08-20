9c32ec12d81f9a3970deea3f6c61b704fc9827af
yuval <yuval@lenovo.2020>
2025-08-20 16:21:38 +0300

    local

--- scrap.py
+++ scrap.py
@@ -1,4 +1,4 @@
 from astro_utils import *
-os.chdir('/media/innereye/KINGSTON/JWST/data/PN-CTR')
-auto_plot('PN-CTR', exp='logPN-CTR_2024-07-23.csv', method='filt05', png='filt1crop_db.jpg', crop='y1=30; y2=4922; x1=13; x2=4834', func=None,
-           adj_args={'factor':1}, fill=False, deband=False, deband_flip=None, pkl=True)
+# os.chdir('/media/innereye/KINGSTON/JWST/data/PN-CTR')
+auto_plot('NGC-6537', exp='*.fits', method='filt05', png='filt1log_db.jpg', crop=False, func=log1,
+           adj_args={'factor':1}, fill=True, deband=True, deband_flip=None, pkl=True)
