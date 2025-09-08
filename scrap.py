3311d301e79eb10a5aaf1b7be413f45373a2b4a9
yuval <yuval@lenovo.2020>
2025-09-08 23:09:50 +0300

    ngc

--- scrap.py
+++ scrap.py
@@ -1,4 +1,4 @@
 from astro_utils import *
 # os.chdir('/media/innereye/KINGSTON/JWST/data/PN-CTR')
-auto_plot('NGC-6537', exp='*.fits', method='filt05', png='filt1log_db.jpg', crop=False, func=log1,
-           adj_args={'factor':1}, fill=True, deband=True, deband_flip=None, pkl=True)
+auto_plot('Ser-emb-11E', exp='*.fits', method='filt05', png='filt2db.jpg', crop=False,
+           adj_args={'factor':2}, fill=True, deband=True, deband_flip=None, pkl=True, func=None)
