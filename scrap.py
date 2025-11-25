25dc40322a0dae0751c4a05d80b268375cca40f4
yuval <yuval@lenovo.2020>
2025-11-25 12:07:52 +0200

    ngc

--- scrap.py
+++ scrap.py
@@ -1,6 +1,4 @@
 from astro_utils import *
 
-os.chdir('/media/innereye/KINGSTON/JWST/data/NGC-2440')
-auto_plot('NGC-2440', method='rrgggbb', deband=True, fill=True, pkl=True, png='rgb4_db_log.jpg', func=log1)
-auto_plot('NGC-2440', method='filt05', deband=False, fill=False, pkl=True, png='filt4_db_log.jpg', func=log1)
-
+auto_plot('LHA-120-N-159-NIRCam', exp='*nircam*.fits',method='filt05', resize=True, deband=False, fill=True, pkl=True, png='nircam_filt.jpg',func=log1)
+print('done')
