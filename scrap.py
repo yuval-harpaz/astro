a3a510aaffc49f98da9c93e904caea4d822cee12
yuval <yuval@debian>
2026-02-19 23:33:41 +0200

    prev

--- scrap.py
+++ scrap.py
@@ -1,5 +1,15 @@
 from astro_utils import *
-os.chdir('/media/innereye/KINGSTON/JWST/data/UHZ1')
-annotate_simbad(None, 'jw06123-o005_t001_miri_f1000w_i2d.fits')
-# auto_plot('LHA-120-N-159-NIRCam', exp='*nircam*.fits',method='filt05', resize=True, deband=False, fill=True, pkl=True, png='nircam_filt.jpg',func=log1)
-# print('done')
+exp = ['jw05437-o003_t003_nircam_clear-f115w_i2d.fits',
+ 'jw05437-o003_t003_nircam_clear-f140m_i2d.fits',
+ 'jw05437-o003_t003_nircam_clear-f182m_i2d.fits',
+ 'jw05437-o003_t003_nircam_clear-f187n_i2d.fits',
+ 'jw05437-o003_t003_nircam_clear-f210m_i2d.fits',
+ 'jw05437-o003_t003_nircam_clear-f300m_i2d.fits',
+ 'jw05437-o003_t003_nircam_clear-f335m_i2d.fits',
+ 'jw05437-o003_t003_nircam_clear-f360m_i2d.fits',
+ 'jw05437-o003_t003_nircam_clear-f444w_i2d.fits',
+ 'jw05437-o003_t003_nircam_f150w2-f162m_i2d.fits',
+ 'jw05437-o003_t003_nircam_f405n-f444w_i2d.fits',
+ 'jw05437-o003_t003_nircam_f444w-f470n_i2d.fits']
+auto_plot('W3-JWST-3A-FINAL', exp=exp[5:9], method='filt05', png='redder2.jpg', crop=False, func=None, adj_args={'factor':2}, fill=True, pkl=False)
+
